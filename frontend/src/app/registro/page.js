4f059185025b9349cb802239895d1d3eee821872
dciceran <gatienza@pioix.edu.ar>
2026-09-23 09:06:26 -0300

    aa

--- frontend/src/app/registro/page.js
+++ frontend/src/app/registro/page.js
@@ -1,96 +1,6 @@
 'use client';
 
 import { useState, useEffect } from 'react';
-
-export default function RegistroPage() {
-    const [username, setUsername] = useState('');
-    const [mail, setMail] = useState('');
-    const [password, setPassword] = useState('');
-    const [fotoPerfil, setFotoPerfil] = useState(null);
-
-    const [fotos, setFotos] = useState([]);
-
-    useEffect(() => {
-        fetch("http://localhost:4000/fotos")
-            .then(response => response.json())
-            .then(data => setFotos(data));
-    }, []);
-
-    function handleSubmit(e) {
-        e.preventDefault();
-
-        fetch("http://localhost:4000/usuarios", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                username: username,
-                mail: mail,
-                password: password,
-                fotoPerfil: fotoPerfil
-            })
-        })
-        .then(response => response.json())
-        .then(data => {
-            console.log("Usuario registrado:", data);
-        });
-
-        console.log("Username:", username);
-        console.log("Mail:", mail);
-        console.log("Password:", password);
-        console.log("Foto Perfil:", fotoPerfil);
-    }
-
-    return (
-        <div>
-            <h1>Registro</h1>
-
-            <form onSubmit={handleSubmit}>
-                <input
-                    type="text"
-                    placeholder="Username"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                />
-
-                <input
-                    type="text"
-                    placeholder="Mail"
-                    value={mail}
-                    onChange={(e) => setMail(e.target.value)}
-                />
-
-                <input
-                    type="password"
-                    placeholder="Password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-
-                <h3>Elige tu foto de perfil:</h3>
-
-                <ul>
-                    {fotos.map(foto => (
-                        <li
-                            key={foto.id}
-                            onClick={() => setFotoPerfil(foto)}
-                        >
-                            <img src={foto.url} />
-                        </li>
-                    ))}
-                </ul>
-
-                <button type="submit">Registrarse</button>
-            </form>
-        </div>
-    );
-}
-
-
-'use client';
-
-import { useState, useEffect } from 'react';
 import { io } from 'socket.io-client';
 
 // Conexión única con Socket.IO
@@ -114,8 +24,8 @@ export default function RegistroPage() {
         });
     }, []);
 
-    function handleSubmit(e) {
-        e.preventDefault();
+    function handleSubmit(event) {
+        event.preventDefault();
 
         // Enviar los datos del registro por Socket.IO
         socket.emit("register", {
@@ -142,21 +52,21 @@ export default function RegistroPage() {
                     type="text"
                     placeholder="Username"
                     value={username}
-                    onChange={(e) => setUsername(e.target.value)}
+                    onChange={(event) => setUsername(event.target.value)}
                 />
 
                 <input
                     type="text"
                     placeholder="Mail"
                     value={mail}
-                    onChange={(e) => setMail(e.target.value)}
+                    onChange={(event) => setMail(event.target.value)}
                 />
 
                 <input
                     type="password"
                     placeholder="Password"
                     value={password}
-                    onChange={(e) => setPassword(e.target.value)}
+                    onChange={(event) => setPassword(event.target.value)}
                 />
 
                 <h3>Elige tu foto de perfil:</h3>
